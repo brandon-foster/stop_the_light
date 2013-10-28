6fc342ed0df30af72653f04b0d9047b831d98a74
Brandon Foster <brandon.foster.vt@gmail.com>
2013-10-28 00:38:45 -0400

    create lightRow function

--- stop_the_light.ino
+++ stop_the_light.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+// array for the pins connected to the LEDs
+int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+// number of elements in ledPins
+int rowLength = sizeof(ledPins) / sizeof(int);
+
+void setup() {
+  // set all the LED pins to OUTPUT
+  for (int i = 0; i < rowLength; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
+  lightRow(100);
+}
+
+/*
+Light up each LED one at a time
+@param delayTime milliseconds to keep each LED on
+*/
+void lightRow(int delayTime) {
   
+  // turn each LED on then off, with specified delay time
+  for (int i = 0; i < rowLength; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    delay(delayTime);
+    digitalWrite(ledPins[i], LOW);
+  }
 }
