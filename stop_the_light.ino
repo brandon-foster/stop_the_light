f32beb22d0aa2f11b75799e74888e08af721768f
Brandon Foster <brandon.foster.vt@gmail.com>
2013-11-03 21:29:47 -0500

    remove for loop, create next() function

--- stop_the_light.ino
+++ stop_the_light.ino
@@ -1,6 +1,15 @@
 // array for the pins connected to the LEDs
 int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+// milliseconds to wait before moving to next LED
+int delayTime = 100;
+
+// the button input pin
+int buttonPin = 11;
+
+// index of the current LED
+int lightPosition = 0;
+
 // number of elements in ledPins
 int rowLength = sizeof(ledPins) / sizeof(int);
 
@@ -9,23 +18,24 @@ void setup() {
   for (int i = 0; i < rowLength; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
+  
+  // set the button pin to INPUT
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
-  lightRow(100);
+  next();
+  delay(delayTime);
 }
 
 /*
-Light up each LED one at a time
-@param delayTime milliseconds to keep each LED on
+Turn off current light then turn on next light
 */
-void lightRow(int delayTime) {
+void next() {
+  // turn off the current LED
+  digitalWrite(ledPins[lightPosition], LOW);
   
-  // turn each LED on then off, with specified delay time
-  for (int i = 0; i < rowLength; i++) {
-    digitalWrite(ledPins[i], HIGH);
-    delay(delayTime);
-    digitalWrite(ledPins[i], LOW);
-  }
+  // turn on the next LED
+  lightPosition = (lightPosition + 1) % (rowLength); // circular indexing
+  digitalWrite(ledPins[lightPosition], HIGH);
 }
